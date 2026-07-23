9835b313751d53b5c4c7f3bd3cb2a4d78cd28ddd
DebHatim <thos.debboun.hatim@gmail.com>
2026-07-23 22:06:24 +0200

    feat: configure Kafka producer bean for OrderDTO messaging

--- order-service/src/main/java/dev/hatimdebboun/orderservice/shared/config/KafkaConfig.java
+++ order-service/src/main/java/dev/hatimdebboun/orderservice/shared/config/KafkaConfig.java
@@ -1,4 +1,46 @@
 package dev.hatimdebboun.orderservice.shared.config;
 
+
+
+import dev.hatimdebboun.orderservice.order.api.OrderDTO;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JacksonJsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
 public class KafkaConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Bean
+    public ProducerFactory<String, OrderDTO> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+
+        // Establecer como atributo el servidor donde funciona Kafka
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+
+        // Serializar la key como String - StringSerializer.class
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        // Serializar el value como JSON - JsonSerializer.class
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JacksonJsonSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    // KafkaTemplate es el objeto que se va a usar en el servicio para publicar mensajes - usa producerFactory
+    public KafkaTemplate<String, OrderDTO> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
 }
