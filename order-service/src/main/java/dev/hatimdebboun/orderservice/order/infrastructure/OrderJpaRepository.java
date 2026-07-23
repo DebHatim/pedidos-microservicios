5f3a2562243cf8a3327752e9862cdd1bd430d7e4
DebHatim <thos.debboun.hatim@gmail.com>
2026-07-23 22:02:48 +0200

    feat: add OrderJpaRepository interface extending JpaRepository

---
+++ order-service/src/main/java/dev/hatimdebboun/orderservice/order/infrastructure/OrderJpaRepository.java
@@ -0,0 +1,6 @@
+package dev.hatimdebboun.orderservice.order.infrastructure;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderJpaRepository extends JpaRepository<OrderJPAEntity, Long> {
+}
