3be68c6633953211ae552a28ff24db8ee235e393
DebHatim <thos.debboun.hatim@gmail.com>
2026-08-04 20:50:44 +0200

    feat: add OrderNotFoundException

---
+++ order-service/src/main/java/dev/hatimdebboun/orderservice/order/domain/OrderNotFoundException.java
@@ -0,0 +1,7 @@
+package dev.hatimdebboun.orderservice.order.domain;
+
+public class OrderNotFoundException extends RuntimeException {
+    public OrderNotFoundException(Long id) {
+        super("Order not found with id: " + id);
+    }
+}
