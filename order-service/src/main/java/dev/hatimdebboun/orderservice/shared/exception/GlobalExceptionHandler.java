e7005d3d5ee734ca5a5b72610d8f45c46473f0f5
DebHatim <thos.debboun.hatim@gmail.com>
2026-08-04 20:51:35 +0200

    refactor: update global exception handler mappings

--- order-service/src/main/java/dev/hatimdebboun/orderservice/shared/exception/GlobalExceptionHandler.java
+++ order-service/src/main/java/dev/hatimdebboun/orderservice/shared/exception/GlobalExceptionHandler.java
@@ -1,4 +1,23 @@
 package dev.hatimdebboun.orderservice.shared.exception;
 
+import dev.hatimdebboun.orderservice.order.domain.OrderNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(OrderNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleOrderNotFound(OrderNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", ex.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, String>> handleException() {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", "An unexpected error occurred"));
+    }
 }
