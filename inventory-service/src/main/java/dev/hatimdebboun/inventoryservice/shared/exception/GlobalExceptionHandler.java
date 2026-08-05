e1a565d8a2d52eaee798c6ae209b3668da88799b
DebHatim <thos.debboun.hatim@gmail.com>
2026-08-05 14:02:37 +0200

    feat: create GlobalExceptionHandler for exception handling

---
+++ inventory-service/src/main/java/dev/hatimdebboun/inventoryservice/shared/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package dev.hatimdebboun.inventoryservice.shared.exception;
+
+import dev.hatimdebboun.inventoryservice.product.domain.ProductNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleProductNotFound(ProductNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", ex.getMessage()));
+    }
+}
