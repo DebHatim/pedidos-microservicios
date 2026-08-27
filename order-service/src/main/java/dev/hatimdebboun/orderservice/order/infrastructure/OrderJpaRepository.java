79c7327768ad14663f7ee1bfb896a1c1fdd9a3d6
DebHatim <thos.debboun.hatim@gmail.com>
2026-08-27 17:59:16 +0200

    refactor: adjust order repository behavior for integration flow

--- order-service/src/main/java/dev/hatimdebboun/orderservice/order/infrastructure/OrderJpaRepository.java
+++ order-service/src/main/java/dev/hatimdebboun/orderservice/order/infrastructure/OrderJpaRepository.java
@@ -4,8 +4,14 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderJpaRepository extends JpaRepository<OrderJpaEntity, Long> {
+
     @EntityGraph(attributePaths = {"items"})
     List<OrderJpaEntity> findAll();
+
+    // Debes añadir esta línea para solucionar el error del test
+    @EntityGraph(attributePaths = {"items"})
+    Optional<OrderJpaEntity> findById(Long id);
 }
